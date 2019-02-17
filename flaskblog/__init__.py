dda8fd3f6eb3dc70de884a5016d61137300d9e26
Pandula <pandula.official@gmail.com>
2019-02-17 20:00:29 +0530

    first commit

---
+++ flaskblog/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '74fa1ee312c7083e97230c98d0364239'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+# Can't put in the top becoz routes use app
+from flaskblog import routes
